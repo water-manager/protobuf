15ffe6118a659a9defe074212273b29aab84bd18
Igorxp5 <rogixp5@gmail.com>
2021-05-02 21:49:55 -0300

    Include build protos
    - Remove out/ from GitIgnore
    - Include compiled proto files

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-if not exist out mkdir out
-if not exist out\java mkdir out\java
-if not exist out\cpp mkdir out\cpp
+if not exist build mkdir build
+if not exist build\java mkdir build\java
+if not exist build\cpp mkdir build\cpp
 
-protoc --java_out=./out/java api.proto
+protoc --java_out=./build/java api.proto
 
-python -m nanopb.generator.nanopb_generator -D ./out/cpp api.proto
+python -m nanopb.generator.nanopb_generator -D ./build/cpp api.proto
