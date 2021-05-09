5d1abd2168dbfa5f98ecae6adf590e25dfe82319
Igorxp5 <rogixp5@gmail.com>
2021-05-09 14:24:34 -0300

    Build to Python

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 if not exist out mkdir out
 if not exist out\java mkdir out\java
 if not exist out\cpp mkdir out\cpp
+if not exist out\python mkdir out\python
 
-protoc --java_out=./out/java --nanopb_out=out/cpp api.proto
+protoc --java_out=./out/java --nanopb_out=out/cpp api.proto --python_out=out/python
 
