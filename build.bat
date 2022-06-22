b5c6b2dd52c427ca52ffa2a48829b6b0f26c2df4
Igorxp5 <rogixp5@gmail.com>
2022-06-22 01:02:04 -0300

    Build for Kotlin

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ if not exist out mkdir out
 if not exist out\java mkdir out\java
 if not exist out\cpp mkdir out\cpp
 if not exist out\python mkdir out\python
+if not exist out\kotlin mkdir out\kotlin
 
-protoc --java_out=./out/java --nanopb_out=out/cpp api.proto --python_out=out/python
-
+protoc --java_out=./out/java --nanopb_out=out/cpp api.proto --python_out=out/python --kotlin_out=out/kotlin
